65572e36c547e28300161453a3affd0dd714ef4f
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-08 13:11:54 +0330

    #

--- JAVASCRIPT/js/scripts.js
+++ JAVASCRIPT/js/scripts.js
@@ -1,7 +1,10 @@
-// Declaration using 'let'
-let x = 10;
-x = 20; // Value can be changed
+var name = "sama";
+var name = "ava";
 
-// Declaration using 'const'
-const y = 5;
-y = 8; // Error: Cannot assign a new value to a constant variable
+console.log(name)
+
+{let age=12;}
+
+console.log(age);
+
+
